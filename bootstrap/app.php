f1326879bf5b3563af8a064fa427d0e1f1049f82
muhanad <3ayary@gmail.com>
2026-03-05 09:26:00 +0200

    error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Middleware\CheckUserRole;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +21,35 @@ return Application::configure(basePath: dirname(__DIR__))
             'IsAdmin' => CheckUserRole::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+
+        $exceptions->render(function (Throwable $e, Request $request) {
+
+            if ($request->is('api/*')) {
+
+                if ($e instanceof ValidationException) {
+                    return response()->json([
+                        'message' => 'Validation failed.',
+                        'errors' => $e->errors(),
+                    ], 422);
+                }
+
+                if ($e instanceof NotFoundHttpException) {
+                    return response()->json([
+                        'message' => 'Not found.',
+                    ], 404);
+                }
+
+                if ($e instanceof AuthenticationException) {
+                    return response()->json([
+                        'message' => 'Unauthorized.',
+                    ], 401);
+                }
+
+                return response()->json([
+                    'message' => 'Something went wrong.',
+                ], 500);
+            }
+        });
+
     })->create();
